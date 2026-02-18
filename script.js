39f002b78732fdf90582186c8d685d34cf21fc7c
MilleFile <173237573+MilleFile@users.noreply.github.com>
2026-02-18 03:51:28 -0500

    Complete step 3 of requirements

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const sketchContainer = document.querySelector("div");
 
-for (let i=0; i < 16; i++) {
-    for (let x=0; x<16; x++) {
+for (let i=0; i < 15; i++) {
+    for (let x=0; x<5; x++) {
         let tempDiv = document.createElement("div");
         tempDiv.textContent = "..b."
         tempDiv.classList.add("unhovered")
         tempDiv.style.display = "flex";
-        tempDiv.style.minWidth = "50px";
-        tempDiv.style.minHeight = "50px";
+        tempDiv.style.minWidth = "10px";
+        tempDiv.style.minHeight = "10px";
+        tempDiv.style.flexGrow = true;
+        tempDiv.addEventListener("mouseenter", () => {
+            tempDiv.classList.remove("unhovered");
+            tempDiv.classList.add("hovered");
+        })
         sketchContainer.appendChild(tempDiv);
     }
 }
