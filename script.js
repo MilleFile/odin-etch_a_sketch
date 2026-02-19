b175f5b238ab90375283e7d62f0c10ac335a4605
MilleFile <173237573+MilleFile@users.noreply.github.com>
2026-02-18 19:35:23 -0500

    Fix grid functionality

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const sketchContainer = document.querySelector("div");
 const buttonSetter = document.querySelector("button");
 
 function resetCanvas(boxCount=10) {
-    if (boxCount > 100) { boxCount = 100};
-    const removeHovered = document.querySelectorAll("div.hovered");
-    const removeUnhovered = document.querySelectorAll("div.unhovered");
-    for (let i = 0; i< removeHovered.length;i++) {
-        sketchContainer.removeChild(removeHovered[i]);
-    }
-    for (let i = 0; i< removeUnhovered.length;i++) {
-        sketchContainer.removeChild(removeUnhovered[i]);
+    if (boxCount > 100) {boxCount = 100};
+    const removeRow = document.querySelectorAll("div.rowManager");
+    for (let i = 0; i< removeRow.length;i++) {
+        sketchContainer.removeChild(removeRow[i]);
     }
     for (let i=0; i < boxCount; i++) {
-        for (let x=0; x<boxCount; x++) {
+        let currentRow = document.createElement("div");
+        currentRow.classList.add("rowManager");
+        currentRow.style.display = "flex";
+        currentRow.style.flex = 1;
+        currentRow.style.flexGrow = 1;
+        currentRow.style.flexDirection = "row";
+        for (let x=0; x < boxCount; x++) {
             let tempDiv = document.createElement("div");
             tempDiv.textContent = ""
             tempDiv.classList.add("unhovered")
@@ -26,8 +28,9 @@ function resetCanvas(boxCount=10) {
                 tempDiv.classList.remove("unhovered");
                 tempDiv.classList.add("hovered");
             })
-            sketchContainer.appendChild(tempDiv);
+            currentRow.appendChild(tempDiv);
         }
+        sketchContainer.appendChild(currentRow);
     }
 }
 resetCanvas();
